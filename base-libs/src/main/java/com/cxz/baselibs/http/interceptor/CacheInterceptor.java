231d295f1fb27e85981f92958fd3327b6a541665
chenxz <563859095@qq.com>
2018-08-21 13:32:41 +0800

    commit

---
+++ base-libs/src/main/java/com/cxz/baselibs/http/interceptor/CacheInterceptor.java
@@ -0,0 +1,46 @@
+package com.cxz.baselibs.http.interceptor;
+
+import com.cxz.baselibs.app.BaseApp;
+import com.cxz.baselibs.utils.NetworkUtil;
+
+import java.io.IOException;
+
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @author chenxz
+ * @date 2018/8/21
+ * @desc CacheInterceptor
+ */
+public class CacheInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        Request request = chain.request();
+        if (!NetworkUtil.isConnected(BaseApp.getContext())) {
+            request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build();
+        }
+        Response response = chain.proceed(request);
+        if (NetworkUtil.isConnected(BaseApp.getContext())) {
+            int maxAge = 0;
+            // 有网络时 设置缓存超时时间0个小时 ,意思就是不读取缓存数据,只对get有用,post没有缓冲
+            response.newBuilder()
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .removeHeader("Retrofit")// 清除头信息，因为服务器如果不支持，会返回一些干扰信息，不清除下面无法生效
+                    .build();
+        } else {
+            // 无网络时，设置超时为4周  只对get有用,post没有缓冲
+            int maxStale = 60 * 60 * 24 * 28;
+            response.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                    .removeHeader("nyn")
+                    .build();
+        }
+        return response;
+    }
+}
