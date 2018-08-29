1e3c1f245a0cc83ec0ca08e16b4171a194962e70
chenxz <563859095@qq.com>
2018-08-29 14:27:26 +0800

    commit md5util

---
+++ base-libs/src/main/java/com/cxz/baselibs/utils/MD5Util.java
@@ -0,0 +1,42 @@
+package com.cxz.baselibs.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author chenxz
+ * @date 2018/8/29
+ * @desc MD5Util
+ */
+public class MD5Util {
+
+    private MD5Util() {
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * MD5加密
+     */
+    public static String encryptMD5(String securityStr) {
+        byte[] data = securityStr.getBytes();
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            md5.update(data);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] resultBytes = md5.digest();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < resultBytes.length; i++) {
+            if (Integer.toHexString(0xFF & resultBytes[i]).length() == 1) {
+                builder.append("0").append(
+                        Integer.toHexString(0xFF & resultBytes[i]));
+            } else {
+                builder.append(Integer.toHexString(0xFF & resultBytes[i]));
+            }
+        }
+        return builder.toString();
+    }
+
+}
