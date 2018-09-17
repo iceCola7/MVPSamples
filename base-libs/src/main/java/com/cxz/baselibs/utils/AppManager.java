ab9d5ef0838204087b0ceab71c6c466b6c735e3f
chenxz <563859095@qq.com>
2018-09-17 10:28:08 +0800

    commit AppManager

---
+++ base-libs/src/main/java/com/cxz/baselibs/utils/AppManager.java
@@ -0,0 +1,104 @@
+package com.cxz.baselibs.utils;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.Stack;
+
+/**
+ * @author chenxz
+ * @date 2018/9/17
+ * @desc Activity 管理类
+ */
+public class AppManager {
+
+    private static AppManager instance;
+    private static Stack<Activity> activityStack;
+
+    private AppManager() {
+    }
+
+    public static AppManager getInstance() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    /**
+     * 添加 Activity 到堆栈
+     */
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 获取当前 Activity （堆栈中最后一个压入的）
+     */
+    public Activity currentActivity() {
+        return activityStack.lastElement();
+    }
+
+    /**
+     * 结束当前 Activity （堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+        if (activityStack == null || activityStack.size() == 0) {
+            return;
+        }
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+            }
+        }
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+    /**
+     * 退出应用程序
+     */
+    public void appExit(Context context) {
+        try {
+            finishAllActivity();
+            ActivityManager activityMgr = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            activityMgr.restartPackage(context.getPackageName());
+            System.exit(0);
+        } catch (Exception e) {
+        }
+    }
+
+}
